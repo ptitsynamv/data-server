31a6d5bd5d7f67cb8d89d3c6c7fffbfb42747ef9
ptitsynamv <ptitsynamv@gmail.com>
2019-09-30 18:16:20 +0300

    keys

--- config/keys.dev.js
+++ config/keys.dev.js
@@ -2,6 +2,6 @@ module.exports = {
     mongoUrl: 'mongodb://admin:admin1@ds135255.mlab.com:35255/antropogenez',
     oauth2Server: {
         url: 'http://localhost:3001',
-        userInfoUrl: '/user/userinfo'
+        userInfoUrl: '/oauth2/userinfo'
     }
 };
